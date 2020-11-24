0c695392c070a7156fdf16814b63bc85bd3abc51
Louis Aude <louis.aude@live.fr>
2020-11-24 14:06:28 +0100

    Added validations, reference keys and create item model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   has_many :invitations_as_sender, source: :invitations, foreign_key: :sender_id
   has_many :invitations_as_receiver, source: :invitations, foreign_key: :receiver_id
   has_many :events
+  has_many :items
 end
