ddc331e299b141f5a65c3ae986dc94409e989b7a
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-24 15:06:47 +0100

    added all event crud methods and views. No styling. Updated footer for easier development

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_event, only: [:edit, :update, :destroy]
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
+
+  def show
+    authorize @event
+  end
 
   def new
     @event = Event.new
@@ -10,27 +14,47 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(strong_params)
     @event.user = current_user
+    authorize @event
+    if @event.save!
+      redirect_to event_path(@event)
+      flash.alert = "Event created"
+    else
+      render :new
+    end
   end
 
   def edit
-
+    authorize @event
   end
 
   def update
-
+    authorize @event
+    if @event.update(strong_params)
+      redirect_to event_path(@event)
+      flash.alert = "Event updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    authorize @event
+    if @event.destroy!
+      redirect_to root_path
+      flash.alert = "Event deleted"
+    else
+      render :show
+      flash.alert = "Event could not be deleted"
+    end
   end
 
   private
 
   def find_event
-
+    @event = Event.find(params[:id])
   end
 
   def strong_params
-    params.require[:event].permit[:title, :location, :time, :description]
+    params.require(:event).permit(:title, :time, :location, :description)
   end
 end
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -5,7 +5,19 @@ class EventPolicy < ApplicationPolicy
     end
   end
 
+  def update?
+    record.user == user
+  end
+
+  def show?
+    user
+  end
+
   def create?
     user
+  end
+
+  def destroy?
+    record.user == user
   end
 end
