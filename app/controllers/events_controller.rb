4d837383d012698252dfc1e8a9078c7d7218b89f
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-24 14:05:41 +0100

    Created event policy, created events controller, views and create form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,36 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_event, only: [:edit, :update, :destroy]
+
+  def new
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+    @event = Event.new(strong_params)
+    @event.user = current_user
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_event
+
+  end
+
+  def strong_params
+    params.require[:event].permit[:title, :location, :time, :description]
+  end
 end
