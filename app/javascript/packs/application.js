17cd21327e7f9a29dc1e18646af5214e9b103927
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-25 14:59:52 +0100

    Added banner upload styling, added datetimepicker with flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { bannerUpload } from "events/edit";
+import { init_flatpickr } from "../plugins/flatpickr"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  bannerUpload();
+  init_flatpickr();
 });
