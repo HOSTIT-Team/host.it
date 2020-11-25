36634434ca6adba1e5720ded74ce8d4db60fb500
Mersad Ajanovic <34665379+flamerged@users.noreply.github.com>
2020-11-25 18:21:35 +0100

    formated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,10 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
- private
+  
+  private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
-
-
-
