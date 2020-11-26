3912c21fc5bb4bd7c964558e2fca098951ecb9bf
Vladimir Tudakov <vladimirtudakov@me.com>
2020-11-26 15:31:48 +0100

    added mapbox javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,6 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
 // External imports
 import "bootstrap";
 
