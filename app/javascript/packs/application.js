a7b623fe8c87ae30d8561763c088787bafe466c8
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-26 03:25:15 +0100

    added guest map section with provisional modal for invite

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { bannerUpload } from "events/edit";
 import { init_flatpickr } from "../plugins/flatpickr"
+import { inviteModal } from "events/show";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   bannerUpload();
   init_flatpickr();
+  inviteModal();
 });
