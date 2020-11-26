1399d7e9928503112f20ffbba0b158f0115d6297
Katharina Hegemer <kathi.hegemer@gmail.com>
2020-11-26 17:08:22 +0100

    further styling on dashboard, moved invitations update route out of events nesting

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events, only: [:show, :new, :create, :edit, :update, :destroy ] do
-    resources :invitations, only: [:update]
+    # resources :invitations, only: [:update]
   end
   resources :dashboard, only: [:index]
   resources :users, only: [:show, :edit, :update, :new]
+  resources :invitations, only: [:update]
 end
