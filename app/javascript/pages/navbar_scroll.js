79283c42847a4b0618cb7bd05efdb6e486c7412d
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-11-29 17:12:53 +0100

    Added shrink to navbar with transition effect

--- app/javascript/pages/navbar_scroll.js
+++ app/javascript/pages/navbar_scroll.js
@@ -1,39 +1,36 @@
 const navbarScroll = () => {
   const navbar = document.querySelector(".navbar-hostit");
-  const navLine = navbar.querySelector(".nav-line");
-  const logo = navLine.querySelector(".navbar-brand img");
-  const avatar = navLine.querySelector(".avatar-card");
-  const dropDown = navLine.querySelector(".avatar-dropdown");
+  const navLine = document.querySelector(".nav-line");
+  const logo = document.querySelector(".brand-logo");
+  const avatar = document.querySelector(".avatar-card");
+  const dropDown = document.querySelector(".dropdown-hostit");
 
-  // const scrollFunction = () => {
-  //   if (
-  //     document.body.scrollTop > 120 ||
-  //     document.documentElement.scrollTop > 120
-  //   ) {
-  //     navbar.style.height = "80px";
-  //     navLine.style.height = "100%";
-  //     navLine.style.alignItems = "center";
-  //     logo.style.height = "60px";
-  //     logo.style.marginTop = "0";
-  //     avatar.style.marginTop = "0!important";
-  //     avatar.style.height = "60px";
-  //     avatar.style.width = "45px";
-  //     dropDown.style.justifyContent = "center";
-  //   } else {
-  //     navbar.style.height = "120px";
-  //     navLine.style.height = "36px";
-  //     logo.style.height = "100px";
-  //     logo.style.marginTop = "0";
-  //     avatar.style.marginTop = "-20px";
-  //     avatar.style.height = "80px";
-  //     avatar.style.width = "60px";
-  //     dropDown.style.justifyContent = "center";
-  //   }
-  // };
+  const scrollFunction = () => {
+    if (
+      document.body.scrollTop > 120 ||
+      document.documentElement.scrollTop > 120
+    ) {
+      navbar.style.height = "80px";
+      navLine.style.height = "80px";
+      logo.style.height = "60px";
+      logo.style.width = "80px";
+      avatar.style.height = "60px";
+      avatar.style.width = "45px";
+      dropDown.style.top = "80px";
+    } else {
+      navbar.style.height = "120px";
+      navLine.style.height = "36px";
+      logo.style.height = "100px";
+      logo.style.width = "130px";
+      avatar.style.height = "80px";
+      avatar.style.width = "60px";
+      dropDown.style.top = "110px";
+    }
+  };
 
-  // window.onscroll = function () {
-  //   scrollFunction();
-  // };
+  window.onscroll = function () {
+    scrollFunction();
+  };
 };
 
 export { navbarScroll };
