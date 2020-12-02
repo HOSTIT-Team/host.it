582ab841e1cd5b7ae44eb2f558ea82a178113376
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-12-02 12:51:05 +0100

    added working ajax for invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -27,16 +27,12 @@ class InvitationsController < ApplicationController
     @invitation.receiver = User.find_by(email: @invitation.receiver_email)
     @invited_already = Invitation.where(event: @event).find_by(receiver_email: @invitation.receiver_email)
     if @invited_already
-      # redirect_to event_path(@event)
-      # flash.alert = "Invitation failed: Guest already invited"
       render json: { success: false, errors: @invitation.errors.messages }, status: :unprocessable_entity 
     elsif @invitation.receiver_email == current_user.email
-      # redirect_to event_path(@event)
-      # flash.alert = "Invitation failed: You can't invite yourself"
       render json: { success: false, errors: @invitation.errors.messages }, status: :unprocessable_entity 
     else
       if @invitation.save
-        render json: { success: true }
+        render json: { invitation: @invitation }
       else
         render json: { success: false, errors: @invitation.errors.messages }, status: :unprocessable_entity 
       end
@@ -48,8 +44,8 @@ class InvitationsController < ApplicationController
     event = @invitation.event
     authorize @invitation
     if @invitation.destroy
-      redirect_to event_path(event)
-      flash.alert = "Invitation deleted"
+      # redirect_to event_path(event)
+      # flash.alert = "Invitation deleted"
     else
       redirect_to event_path(event)
       flash.alert = "Invitation deletion failed"
