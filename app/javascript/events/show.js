a85ab5fe06155f7f89b91bbd3c6301e44c875ec3
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2020-12-03 15:31:50 +0100

    fixed modal button. Modal now closing on click outside

--- app/javascript/events/show.js
+++ app/javascript/events/show.js
@@ -1,18 +1,30 @@
 const inviteModal = () => {
- const openList = document.querySelector(".list-button")
- const closeList = document.querySelector("#close-button")
- const modal = document.querySelector(".invite-modal")
+ const openList = document.querySelectorAll(".list-button");
+ const closeList = document.querySelector("#close-button");
+ const modal = document.querySelector(".invite-modal");
 
  if (openList) {
-  openList.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.style.display = "flex";
-  });
+  openList.forEach(button => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      modal.style.display = "flex";
+    });
+  })
   closeList.addEventListener("click", () => {
     modal.style.display = "none";
   });
  }
+
+ if (modal) {
+  modal.addEventListener("click", event => {
+    if (event.target.className ==   "invite-modal"){
+        document.querySelector(".invite-modal").style.display = "none";
+       }
+    })
+}
 };
+
+
 
 const copyLink = () => {
 
